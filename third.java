ff27768b673700725e93bb446dbc56f9b6d439e2
Roshnisankar <roshnisankarrose@gmail.com>
2024-08-23 19:02:51 +0530

    Add files via upload

---
+++ third.java
@@ -0,0 +1,12 @@
+public class third {
+    public static void main(String[] args) {
+        String name="Sankar";
+        int scored marks in twelfth=527;
+        float percentage=88.8f;
+        System.out.println("name is "+name);
+        System.out.println("scored marks is "+scored marks);
+        System.out.println("percentage is"+percentage );
+
+    }
+    
+}
