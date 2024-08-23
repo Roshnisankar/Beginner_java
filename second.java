77a75ca5218caec263af8f933529f1b0bfe963e4
Roshnisankar <roshnisankarrose@gmail.com>
2024-08-23 19:02:32 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,12 @@
+public class second {
+    public static void main(String[] args) {
+        String name="Roshni";
+        int age =19;
+        float height=134.5f
+        double sal=23000.4567;
+        System.out.println("name is "+name);
+        System.out.println("age is"+age);
+        System.out.println("height"+height+"/n"+"salary"+sal);
+    }
+    
+}
