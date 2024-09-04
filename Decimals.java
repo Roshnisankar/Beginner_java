e3e61184f22a7aa233a2a3235d3957300227730a
Roshnisankar <roshnisankarrose@gmail.com>
2024-09-04 19:05:39 +0530

    Add files via upload

---
+++ Decimals.java
@@ -0,0 +1,9 @@
+public class Decimals {
+    public static void main(String[] args) {
+        float Weight=100.25f;
+        System.out.println("Weight:"+ Weight);
+        double piVal =3.14152337239;
+        System.err.println("Pi Value:"+piVal);
+
+    }
+}
