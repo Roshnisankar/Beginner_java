849383341d58e85200ea17e596de9105e6740e23
Roshnisankar <roshnisankarrose@gmail.com>
2024-09-04 18:45:35 +0530

    Add files via upload

---
+++ five.java
@@ -0,0 +1,11 @@
+public class five {
+ public static void main(String[] args) {
+    String x_value="10";
+    int x=Integer.parseInt(x_value);
+    System.out.println(x);
+    float y=Float.parseFloat(x_value);
+    System.out.println(y);
+    double Z=Double.parseDouble(x_value);
+
+ }    
+}
