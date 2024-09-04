2cff21507634aebce2d1c13c072c119355c2dc98
Roshnisankar <roshnisankarrose@gmail.com>
2024-09-04 18:59:20 +0530

    Add files via upload

---
+++ seven.java
@@ -0,0 +1,10 @@
+public class seven {
+    public static void main(String[] args) {
+        int age=33;
+        System.out.println(age);
+        System.out.println("\nYour age:"+age);
+        int quantity=199;
+        System.out.println("\nTotal Items"+quantity+"\n");
+                
+    }
+}
