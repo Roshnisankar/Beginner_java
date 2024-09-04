afb878f1846e3d7118d6b561367a0565905a2ea3
Roshnisankar <roshnisankarrose@gmail.com>
2024-09-04 19:14:31 +0530

    Add files via upload

---
+++ Intostring.java
@@ -0,0 +1,16 @@
+public class Intostring {
+    public static void main(String[] args) {
+        int x=10;
+        String xValue=Integer.toString(x);
+        System.out.println(xValue);
+        float y=10.3f;
+        String yValue=Float.toString(y);
+        System.out.println(yValue);
+        double z=10.999;
+        String zvalue=Double.toString(z);
+        System.out.println(zValue);
+
+        
+    }
+    
+}
